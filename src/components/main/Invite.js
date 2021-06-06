1439ef13cd0d614ca6d34f977ddd8edae6427916
gelugu <mkraev@vtb.ru>
2021-06-06 14:06:48 +0300

    create location link

--- src/components/main/Invite.js
+++ src/components/main/Invite.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Block } from "../Block";
 import { Content } from "../Content";
 import { Header } from "../Header";
+import { createLink } from "../Link";
 import { Paragraph, ParagraphBold } from "../Paragraph";
 import { Parallax } from "../Parallax";
 import { Wrapper } from "../Wrapper";
 
 export const Invite = () => {
+  const locationLink = "https://art-guslitsa.ru/about";
+
   return (
     <Parallax img="invite.jpg">
       <Wrapper className="invite">
@@ -16,7 +19,9 @@ export const Invite = () => {
             <Header>Злата и Миша</Header>
             <Paragraph>и мы приглашаем вас на нашу свадьбу!</Paragraph>
             <ParagraphBold>17-18 июля 2021 года</ParagraphBold>
-            <ParagraphBold>Арт-усадьба "Гуслица"</ParagraphBold>
+            <ParagraphBold>
+              {createLink('Арт-усадьба "Гуслица"', locationLink)}
+            </ParagraphBold>
             <Paragraph>
               Ниже вы найдёте всю информацию, которая позволит вам разделить с
               нами этот чудесный день в максимально комфортной обстановке.
